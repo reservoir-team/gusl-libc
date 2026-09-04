e2df96261b5476ab97987a1a5365a6c6d25f61f6
Natarizki <natakeren890@gmail.com>
2026-09-05 03:03:48 +0700

    fix: pin gcc-12 to avoid always_inline syslog build error on glibc 2.39

--- scripts/00-host-prep.sh
+++ scripts/00-host-prep.sh
@@ -8,7 +8,8 @@ sudo apt-get update && sudo apt-get upgrade -y
 echo "[00] Installing build dependencies..."
 sudo apt-get install -y \
     build-essential \
-    gcc g++ make \
+    gcc-12 g++-12 \
+    make \
     gawk bison texinfo \
     wget curl \
     git \
@@ -20,6 +21,12 @@ sudo apt-get install -y \
     patch \
     xz-utils \
     pkg-config
+
+echo "[00] Pinning gcc-12/g++-12 as default gcc/g++ (glibc build compatibility)..."
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-12 100
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-12 100
+sudo update-alternatives --set gcc /usr/bin/gcc-12
+sudo update-alternatives --set g++ /usr/bin/g++-12
 
 echo "[00] Preparing work directories..."
 export GUSL_ROOT="${GITHUB_WORKSPACE:-$PWD}"
